a5c4d7bc75a95f55c01bd0235d61dd67389db95f
Shu Horie <r.shu.horie@gmail.com>
2024-11-12 18:20:43 +0900

    fof_cloud.h fo fof_cloud/fof_cloud.h

---
+++ src/fof_cloud.h
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * This file is part of SWIFT.
+ * This file is developed for the on-the-fly cloud finding with FoF
+ * based on Horie+2024.
+ * Copyright (c) 2024 Shu Horie (shorie@ccs.tsukuba.ac.jp).
+ ******************************************************************************/
+#ifndef SWIFT_MASTER_FOF_CLOUD_H
+#define SWIFT_MASTER_FOF_CLOUD_H
+
+/* Config parameters. */
+#include <config.h>
+
+#include "./fof_cloud/fof_cloud.h"
+
+#endif /* SWIFT_MASTER_FOF_CLOUD_H */
